363f9ce6bdd62f6e9a913169eb64ad6d918fc6f5
ismohammad <ishaq.tasleem@gmail.com>
2019-12-18 21:45:34 +0530

    2 files modified

--- src/app/manageuser/manageuser.component.ts
+++ src/app/manageuser/manageuser.component.ts
@@ -8,5 +8,25 @@ import { Component, OnInit } from "@angular/core";
 export class ManageuserComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+ $(document).ready(function() {
+    $('#example').DataTable(
+         {     
+
+      "aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
+        "iDisplayLength": 5
+       } 
+        );
+} );
+
+function checkAll(bx) {
+  var cbs = document.getElementsByTagName('input');
+  for(var i=0; i < cbs.length; i++) {
+    if(cbs[i].type == 'checkbox') {
+      cbs[i].checked = bx.checked;
+    }
+  }
+}
+
+  }
 }
