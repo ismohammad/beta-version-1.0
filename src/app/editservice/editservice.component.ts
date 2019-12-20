8ec4aa483fd423765d98e4015ccf1e13f88b6dc7
ismohammad <ishaq.tasleem@gmail.com>
2019-12-20 22:15:10 +0530

    3 files modified

--- src/app/editservice/editservice.component.ts
+++ src/app/editservice/editservice.component.ts
@@ -10,6 +10,12 @@ export class EditserviceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      $(document).ready(function() {
+      $("#example").DataTable({
+        aLengthMenu: [[5, 10, 25, -1], [5, 10, 25, "All"]],
+        iDisplayLength: 5
+      });
+    }); 
   }
 
 }
